0650cb66f5921ee5cb7558aba71d87d9b45067d7
Saima786-Far <84591699+Saima786-Far@users.noreply.github.com>
2023-12-17 15:10:53 +0530

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,51 @@
+"use strict"; // treat all JS code as newer version .
+//alert("Hello JavaScript");//we are using node.js.not browser
+console.log(3 + 
+    5);
+//code readability should be high 
+console.log("Saima");
+//tc30 MDN Go through should be Important 
+
+let name ="Saima" 
+let age ="23" 
+// let isLoggedIn = False; 
+let city = undefined;
+let area = null;
+
+//Primitive data type
+//number
+//bigInt
+//string
+//boolean
+//null Standalone value ;null is type (null is a object)
+//undefined
+//symbol (unique find) 
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof city);//undefined
+console.log(typeof area);//object
+//practise
+//number
+let length =34;
+let weight = 56;
+//strings
+let color ="yellow";
+let lastName ="Saima";
+//Boleans
+let x= true;
+let y= false;
+//object
+const person = {
+    firstName : "Saima",
+    lastnane : "farheen",
+    age : "23",
+    staus : "single"
+
+};
+//array object 
+const cars=["mercedes", "safari", "alto", "BMW"]
+//Date object 
+const date = new Date ("2022-03-25");
