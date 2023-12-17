cac337d46874443180ab41866b4f1652ccd687fd
Saima786-Far <84591699+Saima786-Far@users.noreply.github.com>
2023-12-17 13:04:46 +0530

    commit

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -3,11 +3,14 @@ let accountEmail = "saimafarheen@google.com";
 var accountPassword = "1234";
 accountCity = "Jamshedpur"
 // accountId = 21 not allowed
-
+let accountState;
 accountEmail = "saims@swd.com"
 accountPassword = "2324324"
 accountCity ="Bengaluru"
 console.log(accountId);
-console.table([accoutnid, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 
+/*
+prefer not to used var because of issues in block scope and functional scope 
 
+*/
