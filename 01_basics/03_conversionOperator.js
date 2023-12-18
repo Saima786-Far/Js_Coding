2ba5d9d0e82d037b046d1b8dc211a530ec0ca471
Saima786-Far <84591699+Saima786-Far@users.noreply.github.com>
2023-12-18 13:01:57 +0530

    Commit

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,64 @@
+let score = "23aabbcc";
+let score1 = 23;
+
+console.log(typeof score);
+console.log(typeof(score1));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+//"33" => 33
+//"2334abe" =>NaN
+//true =>1;false=>0
+//1=true; 0= false
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.group(booleanIsLoggedIn)
+//1 =>true; 0=> false
+//"" =>false
+//"saima"=> true
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+//************************************ Operations *******************************************
+
+ 
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+let str1 = "hello";
+let str2 = "hitesh"
+
+let str3 = str1 +str2 ;
+console.log(str3);
+console.log("1" + 3)
+console.log("1" +2 +3)
+console.log(1 +2 + "3");
+console.log((3+4) * 5 % 3);
+console.log(+true);``
+console.log(+"")
+let gameCounter = 100;
+++gameCounter;
+gameCounter++;
+
+
+
+
+
+
+
+
+
+
+
+
