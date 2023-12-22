0fe0d0594052e58e95b145b4ff9209919f15492e
Saima786-Far <saimafarheen786786@gmail.com>
2023-12-22 22:59:47 +0530

    commit

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,11 +1,23 @@
 // array
-const myArr =[0,1,2,3,4,5] //collection of multiple items //shallow copy 
+const myArr =[0,1,2,3,4,5] //collection of multiple items //shallow copy  same reference point 
+const myheros = [ "saima ", "Farheen"];
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr2[0]);
 
-console.log(myArr[0]);
+//Array methods 
+
+myArr.push(6);
+myArr.push(7);
+myArr.pop();
+myArr.unshift(9);
+myArr.shift();
+console.log(myArr.includes(9))
+console.log(myArr.indexOf(9))
+
+console.log(myArr);
 
 
-
-
+ 
 
 
 
